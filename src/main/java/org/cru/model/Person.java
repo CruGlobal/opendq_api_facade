2cb9d67e419b01324ffee9ab41efe39a75c4ddd2
Bill Randall <william.randall@cru.org>
2014-06-09 11:15:24 -0400

    Update person object to match the transformation file we have so far

--- src/main/java/org/cru/model/Person.java
+++ src/main/java/org/cru/model/Person.java
@@ -5,7 +5,30 @@ package org.cru.model;
  */
 public class Person
 {
-    Address address;
+    private String firstName;
+    private String lastName;
+    private Address address;
+    private String rowId;
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
 
     public Address getAddress()
     {
@@ -15,5 +38,15 @@ public class Person
     public void setAddress(Address address)
     {
         this.address = address;
+    }
+
+    public String getRowId()
+    {
+        return rowId;
+    }
+
+    public void setRowId(String rowId)
+    {
+        this.rowId = rowId;
     }
 }

f76f9db2a05a03ba168a5491b242a4f28f951545
Bill Randall <william.randall@cru.org>
2014-06-10 16:37:15 -0400

    Add multiple tests

--- src/test/java/org/cru/service/MatchingServiceTest.java
+++ src/test/java/org/cru/service/MatchingServiceTest.java
@@ -4,6 +4,7 @@ import org.cru.model.Address;
 import org.cru.model.Person;
 import org.cru.util.OpenDQProperties;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -17,6 +18,20 @@ public class MatchingServiceTest
     private MatchingService matchingService;
     private OpenDQProperties openDQProperties;
 
+    @DataProvider(name = "successfulMatches")
+    private Object[][] successfulMatches()
+    {
+        Person exactMatchFromSoapUI = generatePersonWithDataExactMatchFromSoapUI();
+        Person similarMatchFromSoapUI = generatePersonWithDataSimilarMatchFromSoapUI();
+        Person exactMatchFromJavaTest = generatePersonWithDataExactMatchFromJavaTest();
+
+        return new Object[][] {
+            { exactMatchFromSoapUI, "3" },
+            { exactMatchFromJavaTest, "2" },
+            { similarMatchFromSoapUI, "3" }
+        };
+    }
+
     @BeforeClass
     public void setup()
     {
@@ -27,13 +42,14 @@ public class MatchingServiceTest
         matchingService.setOpenDQProperties(openDQProperties);
     }
 
-    @Test
-    public void testFindMatch() throws Exception
+    @Test(dataProvider = "successfulMatches")
+    public void testFindMatch(Person person, String matchId) throws Exception
     {
-        assertEquals(matchingService.findMatch(createTestPerson(), "test4"), "3");
+        setup();
+        assertEquals(matchingService.findMatch(person, "test1"), matchId);
     }
 
-    private Person createTestPerson()
+    private Person generatePersonWithDataExactMatchFromSoapUI()
     {
         Person testPerson = new Person();
         Address testAddress = new Address();
@@ -43,6 +59,36 @@ public class MatchingServiceTest
 
         testPerson.setFirstName("Test");
         testPerson.setLastName("LastNameTest");
+        testPerson.setAddress(testAddress);
+
+        return testPerson;
+    }
+
+    private Person generatePersonWithDataSimilarMatchFromSoapUI()
+    {
+        Person testPerson = new Person();
+        Address testAddress = new Address();
+
+        testAddress.setAddressLine1("1211 Wee Dr");
+        testAddress.setCity("Orlando");
+
+        testPerson.setFirstName("Testy");
+        testPerson.setLastName("LastNameTest");
+        testPerson.setAddress(testAddress);
+
+        return testPerson;
+    }
+
+    private Person generatePersonWithDataExactMatchFromJavaTest()
+    {
+        Person testPerson = new Person();
+        Address testAddress = new Address();
+
+        testAddress.setAddressLine1("100 Lake Hart Dr");
+        testAddress.setCity("Orlando");
+
+        testPerson.setFirstName("Bill");
+        testPerson.setLastName("Randall");
         testPerson.setAddress(testAddress);
 
         return testPerson;

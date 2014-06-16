2c5aa1d7f909e74613f74b5eb9c8528f6ed2237f
Bill Randall <william.randall@cru.org>
2014-06-16 10:53:09 -0400

    Add matchOrUpdate endpoint

---
+++ src/main/java/org/cru/webservices/MatchOrUpdateResource.java
@@ -0,0 +1,22 @@
+package org.cru.webservices;
+
+import org.cru.model.Person;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by William.Randall on 6/16/14.
+ */
+@Path("/")
+public class MatchOrUpdateResource
+{
+    @Path("/match-or-update")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response matchOrUpdatePerson(Person person)
+    {
+        return Response.ok().build();
+    }
+}

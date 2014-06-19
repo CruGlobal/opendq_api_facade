7148d29d48f4cf68664d47bf8150b037a31dad20
Bill Randall <william.randall@cru.org>
2014-06-19 16:32:52 -0400

    Add more fields to Person

--- src/main/java/org/cru/model/Person.java
+++ src/main/java/org/cru/model/Person.java
@@ -10,6 +10,13 @@ public class Person
     private PersonName name;
     private Address address;
     private String rowId;
+    private String gender;
+    private String employeeNumber;
+    private String accountNumber;
+    private String employeeRelayId;
+    private String relayId;
+    private String emailAddress;
+    private String phoneNumber;
 
     public PersonName getName()
     {
@@ -39,5 +46,75 @@ public class Person
     public void setRowId(String rowId)
     {
         this.rowId = rowId;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public String getEmployeeNumber()
+    {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber)
+    {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber)
+    {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getEmployeeRelayId()
+    {
+        return employeeRelayId;
+    }
+
+    public void setEmployeeRelayId(String employeeRelayId)
+    {
+        this.employeeRelayId = employeeRelayId;
+    }
+
+    public String getRelayId()
+    {
+        return relayId;
+    }
+
+    public void setRelayId(String relayId)
+    {
+        this.relayId = relayId;
+    }
+
+    public String getEmailAddress()
+    {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress)
+    {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber()
+    {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
     }
 }

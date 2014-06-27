7a66bb70c38e605db5fa8f7e55c1072068f26b0d
Bill Randall <william.randall@cru.org>
2014-06-27 11:56:58 -0400

    Update end-to-end test to test mdm values as well

--- src/test/java/org/cru/service/EndToEndServiceTest.java
+++ src/test/java/org/cru/service/EndToEndServiceTest.java
@@ -1,10 +1,13 @@
 package org.cru.service;
 
+import com.infosolve.openmdm.webservices.provider.impl.RealTimeObjectActionDTO;
 import org.cru.cdi.PostalsoftServiceWrapperProducer;
 import org.cru.model.Address;
+import org.cru.model.EmailAddress;
 import org.cru.model.MatchResponse;
 import org.cru.model.Person;
 import org.cru.model.PersonName;
+import org.cru.model.SearchResponse;
 import org.cru.postalsoft.PostalsoftServiceWrapper;
 import org.cru.util.DeletedIndexesFileIO;
 import org.cru.util.OafProperties;
@@ -32,10 +35,10 @@ import static org.testng.Assert.assertNull;
 public class EndToEndServiceTest
 {
     private String filename;
-    private AddService addService;
     private MatchingService matchingService;
     private MatchOrAddService matchOrAddService;
     private DeleteService deleteService;
+    private MatchOrUpdateService matchOrUpdateService;
 
     @BeforeMethod
     public void setup()
@@ -57,9 +60,11 @@ public class EndToEndServiceTest
         DeletedIndexesFileIO deletedIndexesFileIO = new DeletedIndexesFileIO(oafProperties);
 
         deleteService = new DeleteService(deletedIndexesFileIO);
-        addService = new AddService(openDQProperties, addressNormalizationService);
+        AddService addService = new AddService(openDQProperties, addressNormalizationService);
         matchingService = new MatchingService(openDQProperties, deleteService);
         matchOrAddService = new MatchOrAddService(matchingService, addService);
+        UpdateService updateService = new UpdateService(openDQProperties, addressNormalizationService);
+        matchOrUpdateService = new MatchOrUpdateService(matchingService, updateService);
 
         filename = oafProperties.getProperty("deletedIndexFile");
     }
@@ -78,40 +83,97 @@ public class EndToEndServiceTest
     public void endToEndServiceTest() throws Exception
     {
         Person testPerson = createTestPerson();
+        MatchResponse matchResponse = null;
 
         //The person should not exist yet
-        MatchResponse matchResponse = matchingService.findMatch(testPerson, "Match");
-        assertNull(matchResponse);
+        checkPersonNotExists(testPerson);
 
         //This will add the person and return null
-        matchResponse = matchOrAddService.matchOrAddPerson(testPerson);
-        assertNull(matchResponse);
+        checkAddPersonWorked(testPerson);
 
         //Now that the person is added, we should find the person
-        matchResponse = matchOrAddService.matchOrAddPerson(testPerson);
-        assertNotNull(matchResponse);
-        assertEquals(matchResponse.getMatchId(), testPerson.getGlobalRegistryId());
+        checkFindPersonWithMatchOrAdd(testPerson);
 
         //The matching service by itself should also find the person
-        matchResponse = matchingService.findMatch(testPerson, "Match");
-        assertNotNull(matchResponse);
-        assertEquals(matchResponse.getMatchId(), testPerson.getGlobalRegistryId());
+        checkFindPersonWithMatch(testPerson);
+
+        //We should be able to find the index data by Global Registry ID now
+        SearchResponse foundIndex = checkFindById(testPerson.getGlobalRegistryId());
+        String partyId = (String) foundIndex.getResultValues().get("partyId");
+
+        //Make an update to the person
+        //TODO: For now, the update will not work properly because it will find the same row and think it is a conflict
+//        checkUpdatePerson(testPerson, partyId);
 
         //Now we delete the person from the index
-        deleteService.deletePerson(testPerson.getGlobalRegistryId(), null); //TODO: Update this
+        deleteService.deletePerson(testPerson.getGlobalRegistryId(), foundIndex);
 
         //The person should not be found anymore
-        matchResponse = matchingService.findMatch(testPerson, "Match");
+        checkPersonNotExists(testPerson);
+
+        //The record in MDM should have been "deleted"
+        checkPersonDeletedInMdm(testPerson, partyId);
+    }
+
+    private void checkPersonNotExists(Person testPerson) throws Exception
+    {
+        MatchResponse matchResponse = matchingService.findMatch(testPerson, "Match");
         assertNull(matchResponse);
+    }
 
-        //Add the person back with a new row id
-        testPerson.setGlobalRegistryId("9");
-        addService.addPerson(testPerson, "Add");
+    private void checkAddPersonWorked(Person testPerson) throws Exception
+    {
+        assertNull(matchOrAddService.matchOrAddPerson(testPerson));
+    }
 
-        //TODO: Make this work (currently it will still find the original match, and will see it deleted
-        //We should now find the person again
-//        matchResponse = matchingService.findMatch(testPerson, "Match");
-//        assertEquals(matchResponse.getMatchId(), testPerson.getGlobalRegistryId());
+    private void checkFindPersonWithMatchOrAdd(Person testPerson) throws Exception
+    {
+        MatchResponse matchResponse = matchOrAddService.matchOrAddPerson(testPerson);
+        assertNotNull(matchResponse);
+        assertEquals(matchResponse.getMatchId(), testPerson.getGlobalRegistryId());
+    }
+
+    private void checkFindPersonWithMatch(Person testPerson) throws Exception
+    {
+        MatchResponse matchResponse = matchingService.findMatch(testPerson, "Match");
+        assertNotNull(matchResponse);
+        assertEquals(matchResponse.getMatchId(), testPerson.getGlobalRegistryId());
+    }
+
+    private SearchResponse checkFindById(String id) throws Exception
+    {
+        SearchResponse foundIndex = matchingService.findMatchById(id, "MatchId");
+        assertNotNull(foundIndex);
+        assertNotNull(foundIndex.getResultValues().get("partyId"));
+        assertEquals(foundIndex.getId(), id);
+        return foundIndex;
+    }
+
+    private void checkUpdatePerson(Person testPerson, String partyId) throws Exception
+    {
+        List<EmailAddress> emailAddresses = new ArrayList<EmailAddress>();
+        EmailAddress emailAddress = new EmailAddress();
+        emailAddress.setEmail("e2e@blah.com");
+        emailAddress.setId("443432");
+        emailAddresses.add(emailAddress);
+        testPerson.setEmailAddresses(emailAddresses);
+
+        MatchResponse updateResponse = matchOrUpdateService.matchOrUpdatePerson(testPerson);
+        assertNull(updateResponse);  //will be null if updated, not null if it found a conflicting match
+
+        //We should now find the updates
+        RealTimeObjectActionDTO foundPerson = matchingService.findMatchInMdm(partyId);
+        assertNotNull(foundPerson);
+        assertNotNull(foundPerson.getObjectCommunications());
+        assertEquals(foundPerson.getObjectCommunications().getObjectCommunication().size(), 1);
+    }
+
+    private void checkPersonDeletedInMdm(Person testPerson, String partyId) throws Exception
+    {
+        RealTimeObjectActionDTO deletedPerson = matchingService.findMatchInMdm(partyId);
+        assertNotNull(deletedPerson);
+        //The record in MDM should have an Action value of "D"
+        assertEquals(deletedPerson.getObjectEntity().getAction(), "D");
     }
 
     private Person createTestPerson()
@@ -119,18 +181,24 @@ public class EndToEndServiceTest
         Person testPerson = new Person();
 
         Address testAddress = new Address();
-        testAddress.setAddressLine1("End To End Test Address");
-        testAddress.setCity("Greenbay");
+        testAddress.setAddressLine1("E2E4");
+        testAddress.setAddressLine2("E2E5");
+        testAddress.setAddressLine3("E2E6");
+        testAddress.setCity("Dallas");
+        testAddress.setState("TX");
+        testAddress.setZipCode("38437");
+        testAddress.setCountry("USA");
 
         PersonName personName = new PersonName();
-        personName.setFirstName("E2E");
-        personName.setLastName("Test");
+        personName.setTitle("Mr.");
+        personName.setFirstName("EE3");
+        personName.setLastName("EE4");
 
         List<Address> addresses = new ArrayList<Address>();
         addresses.add(testAddress);
         testPerson.setAddresses(addresses);
         testPerson.setName(personName);
-        testPerson.setGlobalRegistryId("8");
+        testPerson.setGlobalRegistryId("afd65af4-hj546fg-xn51rg-5asdf4");
 
         return testPerson;
     }

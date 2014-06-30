b7ee88d698a6be428cb52c0068683201fbd63676
Bill Randall <william.randall@cru.org>
2014-06-30 16:39:54 -0400

    Change Client ID to what Subbu requested

--- src/main/java/org/cru/mdm/MdmConstants.java
+++ src/main/java/org/cru/mdm/MdmConstants.java
@@ -8,6 +8,6 @@ public class MdmConstants
     public static final String SOURCE = "OAF";  //TODO: Will probably come from client
     public static final String USER = "oaf";
     public static final String TYP_ID = "1";  // 1 = Person
-    public static final String CLIENT_ID = "42";  //TODO: What should this be?
+    public static final String CLIENT_ID = "22";
     public static final String JUNK_ID = "0";  // This ID will just be overwritten during insert
 }

47e68ae8ef2a54a82713b842a95677b223d9586f
Bill Randall <william.randall@cru.org>
2014-07-11 16:22:32 -0400

    Add javadoc

--- src/main/java/org/cru/service/AddOrUpdateService.java
+++ src/main/java/org/cru/service/AddOrUpdateService.java
@@ -9,6 +9,12 @@ import javax.inject.Inject;
 import java.net.ConnectException;
 
 /**
+ * Handle logic for either updating or adding a {@link Person}.
+ *
+ * If the person is found and the ID is the same, update the person.
+ * If the person is found and the ID is different, return a conflict response.
+ * If the person is not found, add the person.
+ *
  * Created by William.Randall on 7/11/2014.
  */
 public class AddOrUpdateService

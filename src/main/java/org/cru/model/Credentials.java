87b5673b01d4d4ebbd03f4e32e0aa6e7613c0058
Bill Randall <william.randall@cru.org>
2014-08-11 14:00:18 -0400

    Add entity and primary key for credentials

---
+++ src/main/java/org/cru/model/Credentials.java
@@ -0,0 +1,29 @@
+package org.cru.model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by William.Randall on 8/11/2014.
+ */
+@Entity
+@Table(name = "credentials")
+public class Credentials implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private CredentialsPK credentialsPrimaryKey;
+
+    public CredentialsPK getCredentialsPrimaryKey()
+    {
+        return credentialsPrimaryKey;
+    }
+
+    public void setCredentialsPrimaryKey(CredentialsPK credentialsPrimaryKey)
+    {
+        this.credentialsPrimaryKey = credentialsPrimaryKey;
+    }
+}
